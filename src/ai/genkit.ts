e7862ea7d66287a7f500f3d4b6191db8f42ae933
Mayank Dayal <mayankdl203@gmail.com>
2025-09-14 08:20:49 +0000

    still saem error is coming

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,11 +1,9 @@
-require('dotenv').config({ path: '.env.local' });
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
   plugins: [
     googleAI({
-      model: 'gemini-1.5-flash',
       apiKey: process.env.GEMINI_API_KEY,
     }),
   ],
